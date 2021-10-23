d4208229eebff4a18a335e4c43bf80495892740a
Daniel Bou <danielbou@outlook.com.br>
2021-10-23 15:13:59 -0300

    Service Descovery com Eureka

--- loja/src/main/java/br/com/alura/microservice/loja/LojaApplication.java
+++ loja/src/main/java/br/com/alura/microservice/loja/LojaApplication.java
@@ -1,10 +1,20 @@
 package br.com.alura.microservice.loja;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class LojaApplication {
+	
+	@Bean
+	@LoadBalanced // Faz o RestTemplate buscar as informações no Euroka
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LojaApplication.class, args);
