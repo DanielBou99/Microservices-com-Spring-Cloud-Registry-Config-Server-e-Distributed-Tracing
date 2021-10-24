98294dc1fe484745a030477717acb9a6d5c3b05b
Daniel Bou <danielbou@outlook.com.br>
2021-10-24 16:12:40 -0300

    Log com Papertrail e Spring Sleuth

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,5 +1,7 @@
 package br.com.alura.microservice.loja.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -15,13 +17,20 @@ import br.com.alura.microservice.loja.modelo.Compra;
 @Service
 public class CompraService {
 	
+	
+	private static final Logger LOG = LoggerFactory.getLogger(CompraService.class);
+
 	@Autowired
 	private FornecedorClient fornecedorClient;
 	
 	public Compra realizaCompra(CompraDTO compra) {
 		
+		final String estado = compra.getEndereco().getEstado();
+		
+		LOG.info("Buscando informações do fornecedor de {}.", estado);
 		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
 		
+		LOG.info("Realizando um pedido.");
 		InfoPedidoDTO pedido = fornecedorClient.realizaPedido(compra.getItens());
 
 		Compra compraSalva = new Compra();
